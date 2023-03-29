93b05698c75b4ec226368b71d029473fdc74005b
playerzer0-ui <playerzero745@gmail.com>
2023-03-29 16:31:41 +0100

    size testing implemented along with some pre-made Tasks

--- src/tests/BoundedPriorityQueueSetTest.java
+++ src/tests/BoundedPriorityQueueSetTest.java
@@ -1,7 +1,46 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import utils.BoundedPriorityQueueSet;
+import utils.Task;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoundedPriorityQueueSetTest {
+    Task x = new Task("jer", "do class", LocalDate.parse("2023-04-02"));
+    Task y = new Task("jer", "do class", LocalDate.parse("2023-04-04"));
+    Task z = new Task("jer", "do class", LocalDate.parse("2023-04-03"));
+    Task a = new Task("jer", "do class", LocalDate.parse("2023-04-01"));
+    Task b = new Task("jer", "do class", LocalDate.parse("2023-04-05"));
+    Task c = new Task("jer", "do class", LocalDate.parse("2023-04-06"));
+    Task d = new Task("jer", "do class", LocalDate.parse("2023-04-07"));
+    Task e = new Task("jer", "do class", LocalDate.parse("2023-04-08"));
+    Task f = new Task("jer", "do class", LocalDate.parse("2023-04-09"));
+    Task g = new Task("jer", "do class", LocalDate.parse("2023-04-10"));
 
+    /**
+     * size method, normal scenario
+     */
+    @Test
+    void size_normal() {
+        BoundedPriorityQueueSet q = new BoundedPriorityQueueSet();
+        int exp = 2;
+        q.add(x);
+        q.add(y);
+        int act = q.size();
+        assertEquals(exp, act);
+    }
+
+    /**
+     * size method, but it's empty
+     */
+    @Test
+    void size_empty() {
+        BoundedPriorityQueueSet q = new BoundedPriorityQueueSet();
+        int exp = 0;
+        int act = q.size();
+        assertEquals(exp, act);
+    }
 }
